a6248adece93e3dbe394e044104fa98023a8f8ef
Luando <lofame@gmail.com>
2022-08-09 22:34:50 -0300

    Comecando a implementar o tabuleiro

---
+++ xadrez-console/tabuleiro/Peca.cs
@@ -0,0 +1,19 @@
+﻿
+
+namespace tabuleiro
+{
+     class Peca
+    {
+        public Pocisao pocisao { get; set; }
+        public Cor cor { get; protected set; }
+        public int qtdMovimentos { get; protected set; }
+        public Tabuleiro tab { get; set; }
+
+        public Peca(Pocisao pocisao, Cor cor, Tabuleiro tab)
+        {
+            this.pocisao = pocisao;
+            this.cor = cor;
+            this.tab = tab;
+        }
+    }
+}
---
+++ xadrez-console/tabuleiro/Pocisao.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tabuleiro
+{
+    internal class Pocisao
+    {
+        public int coluna { get; set; }
+        public int linha { get; set; }
+
+        public Pocisao(int coluna, int linha)
+        {
+            this.coluna = coluna;
+            this.linha = linha;
+        }
+    }
+}
